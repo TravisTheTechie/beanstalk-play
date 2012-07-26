2479ea09a78454abd37563e240a8ab1281883ca3
Travis Smith <travis@legomaster.net>
2012-07-26 11:36:57 -0400

    Now working.

--- project/Build.scala
+++ project/Build.scala
@@ -9,10 +9,12 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+      "com.github.play2war" %% "play2-war-core" % "0.6"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here      
+      resolvers += "Play2war plugins release" at "http://repository-play-war.forge.cloudbees.com/release/" 
     )
 
 }
